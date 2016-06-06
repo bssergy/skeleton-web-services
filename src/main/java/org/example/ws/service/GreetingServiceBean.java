70c7fc1285ccf1112be9ebe295f570c363aba42e
Sergiy Brao <sbrao@ancestry.com>
2016-06-06 12:20:56 +0300

    Added Declarative Transaction Management

--- src/main/java/org/example/ws/service/GreetingServiceBean.java
+++ src/main/java/org/example/ws/service/GreetingServiceBean.java
@@ -4,8 +4,13 @@ import org.example.ws.model.Greeting;
 import org.example.ws.repository.GreetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(
+        propagation = Propagation.SUPPORTS,
+        readOnly = true)
 public class GreetingServiceBean implements GreetingService {
 
     @Autowired
@@ -24,6 +29,9 @@ public class GreetingServiceBean implements GreetingService {
     }
 
     @Override
+    @Transactional(
+            propagation = Propagation.REQUIRED,
+            readOnly = false)
     public Greeting create(Greeting greeting) {
         if (greeting.getId() != null) {
             // Cannot create Greeting with specified ID value
@@ -31,10 +39,19 @@ public class GreetingServiceBean implements GreetingService {
         }
 
         Greeting savedGreeting = greetingRepository.save(greeting);
+
+        // Illustrate Tx rollback
+        if (savedGreeting.getId() == 4L) {
+            throw new RuntimeException("Roll me back!");
+        }
+
         return savedGreeting;
     }
 
     @Override
+    @Transactional(
+            propagation = Propagation.REQUIRED,
+            readOnly = false)
     public Greeting update(Greeting greeting) {
         Greeting greetingPersisted = findOne(greeting.getId());
         if (greetingPersisted == null) {
@@ -47,6 +64,9 @@ public class GreetingServiceBean implements GreetingService {
     }
 
     @Override
+    @Transactional(
+            propagation = Propagation.REQUIRED,
+            readOnly = false)
     public void delete(Long id) {
         greetingRepository.delete(id);
     }
